a723c96cb683675efad5b44d88be8f00ee7ca08a
Minh Tuan <tuan0971618414@gmail.com>
2022-06-23 09:22:20 +0700

    a

--- 102200292_LeTuMinhTuan/GUI/DetailForm.cs
+++ 102200292_LeTuMinhTuan/GUI/DetailForm.cs
@@ -1,4 +1,5 @@
-﻿using _102200292_LeTuMinhTuan.DTO;
+﻿using _102200292_LeTuMinhTuan.BLL;
+using _102200292_LeTuMinhTuan.DTO;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,11 +14,44 @@ namespace _102200292_LeTuMinhTuan.GUI
 {
     public partial class DetailForm : Form
     {
-        public DetailForm()
+        public delegate void Del();
+        public Del d;
+        public string MaSinhVien { get; set; }
+        public string MaHocPhan { get; set; }
+        public DetailForm(string MaSinhVien, string MaHocPhan)
         {
+            this.StartPosition = FormStartPosition.CenterScreen;
             InitializeComponent();
-        }
+            this.cbbHocPhan.Items.AddRange(BLL_QL.Instance.GetCBBHocPhan().ToArray());
+            this.MaSinhVien = MaSinhVien;
+            this.MaHocPhan = MaHocPhan;
 
+        }
+        public void SetGUI()
+        {
+            if(BLL_QL.Instance.GetSinhVien(this.MaSinhVien, this.MaHocPhan) != null)
+            {
+                SinhVien data = BLL_QL.Instance.GetSinhVien(this.MaSinhVien, this.MaHocPhan);
+                this.txtMSSV.Text = data.MaSinhVien;
+                this.txtTen.Text = data.TenSinhVien;
+                this.cbbLopSH.Text = data.LopSinhHoat;
+                this.dateTimePicker1.Value = data.NgayThi;
+                this.txtDiemBT.Text = data.DiemBaiTap.ToString();
+                this.txtDiemGK.Text = data.DiemGiuaKi.ToString();
+                this.txtDiemCK.Text = data.DiemCuoiKi.ToString();
+                this.txtTongKet.Text = data.TongKet.ToString();
+                if (data.GioiTinh) this.radioMale.Checked = true;
+                else this.radioFemale.Checked = false;
+                foreach(CBBItem i in this.cbbHocPhan.Items)
+                {
+                    if (i.Key.Equals(data.MaHocPhan))
+                    {
+                        this.cbbHocPhan.SelectedItem = i;
+                        break;
+                    }
+                }
+            }
+        }
         private void btnOK_Click(object sender, EventArgs e)
         {
             SinhVien sv = new SinhVien
@@ -29,8 +63,12 @@ namespace _102200292_LeTuMinhTuan.GUI
                 TenHocPhan = ((CBBItem)this.cbbHocPhan.SelectedItem).Value,
                 GioiTinh = this.radioMale.Checked,
                 DiemBaiTap = Convert.ToDouble(this.txtDiemBT.Text),
-                DiemGiuaKi = Convert.ToDouble(this.txtDiemBT.Text),
-            }
+                DiemGiuaKi = Convert.ToDouble(this.txtDiemGK.Text),
+                DiemCuoiKi = Convert.ToDouble(this.txtDiemCK.Text),
+                NgayThi = this.dateTimePicker1.Value,
+                TongKet = Convert.ToDouble(this.txtTongKet.Text),
+            };
+
         }
     }
 }
